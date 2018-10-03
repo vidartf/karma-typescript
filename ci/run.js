0fc156a08f32e58a7ae0682e8d91ee2080324e76
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-10-03 15:32:17 +0200

    Make run script less triangular

--- ci/run.js
+++ ci/run.js
@@ -2,47 +2,45 @@ var spawn = require("cross-spawn");
 
 var command = "npm run dev:ci";
 
-// The pyramid of doom :( If you have a better idea, (which doesn't
-// involve wait.for / node-gyp / fibers) please create a pull request,
-// but bear in mind that this has to run on windows machines as well...
-execute("examples/angular2", command, function() {
-    execute("examples/angularjs", command, function() {
-        execute("examples/mocha", command, function() {
-            execute("examples/typescript-1.6.2", command, function() {
-                execute("examples/typescript-latest", command, function() {
-                    
-                    execute("tests/integration-1.8.10", command, function() {
-                        execute("tests/integration-1.8.10", command + ":angular2", function() {
-                            execute("tests/integration-1.8.10", command + ":react", function() {
-                                
-                                execute("tests/integration-latest", command, function() {
-                                    execute("tests/integration-latest", command + ":angular2", function() {
-                                        execute("tests/integration-latest", command + ":core", function() {
-                                            execute("tests/integration-latest", command + ":emptyfile", function() {
-                                                execute("tests/integration-latest", command + ":no-module", function() {
-                                                    execute("tests/integration-latest", command + ":react", function() {
-                                                        //
-                                                    });
-                                                });
-                                            });
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    });
-});
+var commands = [
+    ["examples/angular2", command],
+    ["examples/angularjs", command],
+    ["examples/mocha", command],
+    ["examples/typescript-1.6.2", command],
+    ["examples/typescript-latest", command],
+
+    ["tests/integration-1.8.10", command],
+    ["tests/integration-1.8.10", command + ":angular2"],
+    ["tests/integration-1.8.10", command + ":react"],
+
+    ["tests/integration-latest", command],
+    ["tests/integration-latest", command + ":angular2"],
+    ["tests/integration-latest", command + ":core"],
+    ["tests/integration-latest", command + ":emptyfile"],
+    ["tests/integration-latest", command + ":no-module"],
+    ["tests/integration-latest", command + ":react"],
+];
 
 function execute(dir, cmd, callback) {
-    
+
     var x = cmd.split(" ");
     var runner = spawn(x[0], x.splice(1), { cwd: dir, stdio: "inherit" });
-        
+
     runner.on("close", function(code) {
         callback(code);
     });
 }
+
+
+// FIXME: Try to avoid recursing for every step
+var i = 0;
+function step(code) {
+    if (i >= commands.length) return;
+    var dir = commands[i][0];
+    var cmd = commands[i][1];
+    ++i;
+    // TODO: Use code?
+    execute(dir, cmd, step);
+}
+
+step();
